cb57bcf4c5cad3311bddf0f551f1a3139c0fc0a0
hpeffley <hpeffley@dtcc.edu>
2016-08-29 09:08:25 -0400

    Added an event handler to the button

--- CIS209-29Aug16-1/Form1.cs
+++ CIS209-29Aug16-1/Form1.cs
@@ -16,5 +16,10 @@ namespace CIS209_29Aug16_1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
     }
 }
